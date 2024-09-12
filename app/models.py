105a2d19071ef4534599f44ee33ae2b51f04a551
NucyLoodle <lucymeredith3@gmail.com>
2024-09-12 12:38:07 +0100

    load next page when user logged in

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
+from app import db, login
 
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
