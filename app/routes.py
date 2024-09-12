a3a04229da476071262618d44a7040506771cefa
NucyLoodle <lucymeredith3@gmail.com>
2024-09-12 13:50:14 +0100

    build route for register

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, url_for, request
 from urllib.parse import urlsplit
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 import sqlalchemy as sa
 from app import db
 from app.models import User
@@ -34,6 +34,20 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('profile'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 @app.route('/profile')
 def profile():
