fe7683a7ca759cce6b3f0702435009b60904d6c6
NucyLoodle <lucymeredith3@gmail.com>
2024-09-12 11:58:50 +0100

    write methods for hashing password

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
@@ -13,3 +15,9 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+        
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
